0ac40e4746b3307f0e8c18049f9ec1ab4a23ff36
Sidljackson <sid@span-eng.com>
2024-10-17 12:06:59 +1000

    adding a splash of colour

--- processor/ui.py
+++ processor/ui.py
@@ -55,7 +55,7 @@ def construct_ui(processor):
                     "tankLevelTriggers", "Tank Level Triggers (%)",
                     min_val=0, max_val=100, step_size=1, dual_slider=True,
                     inverted=True, icon="fa-regular fa-tank-water", show_activity=True,
-                    default_val=[50, 90]
+                    default_val=[50, 90],colours = [ui.Colour.yellow, ui.Colour.blue, ui.Colour.green]
                 ),
                 # ui.Slider("levelAlert", "Low Level Alert (%)", 
                 #     min_val=0, max_val=100, step_size=1, dual_slider=False,
