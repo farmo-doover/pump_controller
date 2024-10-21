b206a812fc648d548c0ebe71039aa324964eaec0
Sidljackson <sid@span-eng.com>
2024-10-21 13:30:47 +1000

    a

--- processor/ui.py
+++ processor/ui.py
@@ -82,10 +82,10 @@ def construct_ui(processor):
         # ui.TextVariable("imei", "IMEI"),
         ui.HiddenValue("_pumpState", show_activity=False),
         ui.ConnectionInfo(name="connectionInfo",
-            connection_type=ui.ConnectionType.other,
+            # connection_type=ui.ConnectionType.periodic,
             # connection_period=processor.get_connection_period(),
             # next_connection=processor.get_connection_period(),
-            # offline_after=(60 * 60), # 1 hour
+            offline_after=(60 * 60), # 1 hour
             # allowed_misses=4,
         )
     )
