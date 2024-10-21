8253722b678dda86753bb5294d0083950f79711d
Sidljackson <sid@span-eng.com>
2024-10-22 09:44:10 +1000

    a

--- processor/target.py
+++ processor/target.py
@@ -115,6 +115,7 @@ class target(ProcessorBase):
         return pump_state_obj['currentValue']
 
     def set_pump_state(self, state):
+        logging.info(f"Setting pump state to: {state}")
         # self.ui_manager.coerce_command("_pumpState", state)
         ## Update the 'startStopNow' button
         ss_button = self.ui_manager.get_interaction("startStopNow")
